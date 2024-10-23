cc6548f8443afba3d092e2c8ee78d0b160f13158
SinergiseTeam6 <js+github@sinergise.com>
2024-10-23 12:30:28 +0000

    Update sources to version [v1.12.8]

--- src/VERSION.js
+++ src/VERSION.js
@@ -1,1 +1,1 @@
-export const VERSION_INFO = { tag: 'v1.12.7', branch: 'v1.12.7', commit: '5ab1679d700686d70dab9a9bca6ee917466277e6' }
+export const VERSION_INFO = { tag: 'v1.12.8', branch: 'v1.12.8', commit: '5ab1679d700686d70dab9a9bca6ee917466277e6' }
