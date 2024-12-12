f0e31e810334b8d6e16cc301fee8984302417e5d
SinergiseTeam6 <js+github@sinergise.com>
2024-12-12 12:34:52 +0000

    Update sources to version [v1.13.8]

--- src/VERSION.js
+++ src/VERSION.js
@@ -1,1 +1,1 @@
-export const VERSION_INFO = { tag: 'v1.13.7', branch: 'v1.13.7', commit: 'e363b54a00a520cc860a24c4babae9de3a575860' }
+export const VERSION_INFO = { tag: 'v1.13.8', branch: 'v1.13.8', commit: '4a20293418b65bf4ae5e346954500bf789dd1383' }
--- src/api/OData/ODataHelpers.js
+++ src/api/OData/ODataHelpers.js
@@ -42,7 +42,6 @@ import {
   AttributeOnlineValues,
   AttributeOperationalModeValues,
   AttributeOrbitDirectionValues,
-  AttributePlatformSerialIdentifierValues,
   AttributePolarisationChannelsValues,
   FormatedAttributeNames,
   ODataAttributes,
@@ -138,15 +137,6 @@ export const getDatasetIdFromProductType = (productType, attributes) => {
     }
     if (/DGE_90/.test(productType)) {
       return PRODUCT_TYPE_TO_DATASETID['COP-DEM_GLO-90-DGED'];
-    }
-  }
-
-  if ([S2_L1C_CDAS, S2_L2A_CDAS].includes(PRODUCT_TYPE_TO_DATASETID[productType])) {
-    const platformSerialIdentifier = attributes.find(
-      (attribute) => attribute?.Name === AttributeNames.platformSerialIdentifier,
-    );
-    if (platformSerialIdentifier?.Value === AttributePlatformSerialIdentifierValues.S2C.value) {
-      return null;
     }
   }
 
