3049f652df877aea76e56b3c328b1b6ddf325505
SinergiseTeam6 <js+github@sinergise.com>
2025-06-11 13:31:57 +0000

    Update sources to version [v1.17.2]

--- src/VERSION.js
+++ src/VERSION.js
@@ -1,1 +1,1 @@
-export const VERSION_INFO = { tag: 'v1.17.1', branch: 'v1.17.1', commit: 'ca7aae9981a0665c831807770e98cdf982c90064' }
+export const VERSION_INFO = { tag: 'v1.17.2', branch: 'v1.17.2', commit: '4b5f3dd13783c9d4a4d3ff13a70609ca2185b3b5' }
